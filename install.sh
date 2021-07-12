d6d60d2b171bb13b28bd254512d070023391b68a
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-12 23:27:46 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+termux-wake-lock
+
+pkg upgrade -y && pkg i python inotify-tools jq cronie -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
+
+~/ipcamgram/tgsend.py "me" "~/ipcamgram/README.md" "authorization successful"
