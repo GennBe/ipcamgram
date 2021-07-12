6dee222b0fb11e4086c5ad6845958a04a24c88d6
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 02:47:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 termux-wake-lock
 
-pkg upgrade -y && pkg i python inotify-tools jq cronie -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
+pkg i python inotify-tools jq cronie -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
 
 ~/ipcamgram/tgsend.sh "me" ~/ipcamgram/README.md "authorization successful"
