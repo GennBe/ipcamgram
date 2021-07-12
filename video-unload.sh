d333ea3920d164cc2e9ab55a76aa5bc521c2188d
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-12 18:28:29 +0300

    Create video-unload.sh

---
+++ video-unload.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+
+. ~/ipcamgram/ipcamgram-config
+
+if [ $NET_TEST -eq 0 ]
+        then
+            bot_message "Выгружаю из $VIDEO_DIR"
+            echo "Выгружаю из $VIDEO_DIR"
+            ~/ipcamgram/tgsend.py "$CHAT_ID" "$VIDEO_DIR/*.$FORMAT" "$CAPTION"
+            rm -rf $VIDEO_DIR/*
+        else
+            echo "Нет доступа к Telegram"
+        fi
