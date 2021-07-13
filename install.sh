dc18a42cfe8d5b4ddcea224054268fdc1b6d2749
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 16:52:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ termux-wake-lock
 
 echo "y" | termux-setup-storage
 
-pkg i termux-api python inotify-tools jq cronie -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
+pkg i termux-api python inotify-tools jq -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
 
 ! [ -d ~/.termux/boot ] && mkdir -p ~/.termux/boot && cp ~/ipcamgram/boot-ip-webcam.sh ~/.termux/boot/ && ! [ -d ~/storage/shared/ipwebcam_videos ] && mkdir -p ~/storage/shared/ipwebcam_videos
 
