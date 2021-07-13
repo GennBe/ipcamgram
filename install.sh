b9bfa919c2bab6fa5977777e237be58bbe3585ec
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 15:16:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ termux-wake-lock
 
 pkg i termux-api python inotify-tools jq cronie -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
 
-! [ -d ~/.termux/boot/ ] && mkdir -p ~/.termux/boot/ && cp ~/ipcamgram/boot-ip-webcam.sh ~/.termux/boot/
+! [ -d ~/.termux/boot ] && mkdir -p ~/.termux/boot && cp ~/ipcamgram/boot-ip-webcam.sh ~/.termux/boot/
+
+echo "y" | termux-setup-storage
 
 ~/ipcamgram/tgsend.sh "me" ~/ipcamgram/README.md "authorization successful"
