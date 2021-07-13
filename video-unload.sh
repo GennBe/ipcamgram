b3533fa5dbd0feb6ce6c1513d63078bd230f2886
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 12:21:20 +0300

    Update video-unload.sh

--- video-unload.sh
+++ video-unload.sh
@@ -6,6 +6,6 @@ if [ $(ls $VIDEO_DIR | wc -l) -ge 1 ] && [ $NET_TEST -eq 0 ]
 then
     bot_message "Выгружаю из $VIDEO_DIR"
     echo "Выгружаю из $VIDEO_DIR"
-    ~/ipcamgram/tgsend.sh "$CHAT_ID" "$VIDEO_DIR/*.$FORMAT" "$CAPTION"
+    ~/ipcamgram/tgsend.sh "$CHAT_ID" "$VIDEO_DIR/*" "$CAPTION"
     rm -rf $VIDEO_DIR/*
 fi
