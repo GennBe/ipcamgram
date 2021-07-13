eeefb4630fc517b8edb69b8f17f1515484422fc7
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 18:30:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ echo "y" | termux-setup-storage
 
 pkg i termux-api python inotify-tools jq -y && pip install -U pyrogram tgcrypto && pkg clean && pkg autoclean
 
-! [ -d ~/.termux/boot ] && mkdir -p ~/.termux/boot && cp ~/ipcamgram/boot-ip-webcam.sh ~/.termux/boot/ && ! [ -d ~/storage/shared/ipwebcam_videos ] && mkdir -p ~/storage/shared/ipwebcam_videos
+! [ -d ~/.termux/boot ] && mkdir -p ~/.termux/boot
+
+cp ~/ipcamgram/boot-ip-webcam.sh ~/.termux/boot/
+
+! [ -d ~/storage/shared/ipwebcam_videos ] && mkdir -p ~/storage/shared/ipwebcam_videos
 
 ~/ipcamgram/tgsend.sh "me" ~/ipcamgram/README.md "authorization successful"
