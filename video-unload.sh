ee2a5b4e747181034dd5c4d0890050e987f3be02
GennBe <87283011+GennBe@users.noreply.github.com>
2021-07-13 12:11:40 +0300

    Update video-unload.sh

--- video-unload.sh
+++ video-unload.sh
@@ -2,12 +2,10 @@
 
 . ~/ipcamgram/ipcamgram-config
 
-if [ $NET_TEST -eq 0 ]
+if [ $(ls $VIDEO_DIR | wc -l) -ge 1 ] && [ $NET_TEST -eq 0 ]
 then
     bot_message "Выгружаю из $VIDEO_DIR"
     echo "Выгружаю из $VIDEO_DIR"
     ~/ipcamgram/tgsend.sh "$CHAT_ID" "$VIDEO_DIR/*.$FORMAT" "$CAPTION"
     rm -rf $VIDEO_DIR/*
-else
-    echo "Нет доступа к Telegram"
 fi
